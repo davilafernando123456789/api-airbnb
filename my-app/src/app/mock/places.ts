d08c16232dc031fc4392810e3e3007a3df5e411b
davilafernando123456789 <91484392+davilafernando123456789@users.noreply.github.com>
2023-12-15 14:18:34 -0500

    Add files via upload

---
+++ my-app/src/app/mock/places.ts
@@ -0,0 +1,136 @@
+export const places = {
+    "places": [
+        {
+            "name": "Modern Art Loft",
+            "rating": 4.5,
+            "location": {
+                "country": "United States",
+                "city": "New York"
+            },
+            "image_url": "https://cdn.tiqets.com/wordpress/blog/wp-content/uploads/2017/08/03134557/24-hours-in-new-york-1-1024x570.jpg",
+            "date": "2023-01-15",
+            "price": 120.99,
+            "latitude": 40.730610,
+            "longitude": -73.935242 
+        },
+        {
+            "name": "Adventure National Park Lodge",
+            "rating": 4.2,
+            "location": {
+                "country": "Canada",
+                "city": "Vancouver"
+            },
+            "image_url": "https://www.vmcdn.ca/f/files/via/images/city-images/downtown-vancouver-sunny-day-shore.jpg;w=960;h=586;mode=crop",
+            "date": "2023-02-20",
+            "price": 95.75,
+            "latitude": 49.282730, 
+            "longitude": -123.120735
+        },
+        {
+            "name": "Seafront Restaurant Retreat",
+            "rating": 4.8,
+            "location": {
+                "country": "United Kingdom",
+                "city": "London"
+            },
+            "image_url": "https://plus.unsplash.com/premium_photo-1661964203376-9aeba8cdbd23?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bG9uZG9uJTJDJTIwdW5pdGVkJTIwa2luZ2RvbXxlbnwwfHwwfHx8MA%3D%3D",
+            "date": "2023-03-10",
+            "price": 225.5,
+            "latitude": 51.507322,
+            "longitude": -0.127647
+        },
+        {
+            "name": "Mountain Trail Cabin",
+            "rating": 4,
+            "location": {
+                "country": "Australia",
+                "city": "Sydney"
+            },
+            "image_url": "https://images.squarespace-cdn.com/content/v1/55ee34aae4b0bf70212ada4c/1577545161018-1F9Z9ZZQG9JO2O4WCWQX/keith-zhu-qaNcz43MeY8-unsplash+%281%29.jpg?format=1500w",
+            "date": "2023-04-05",
+            "price": 75.99,
+            "latitude": -33.868800, 
+            "longitude": 151.209290
+        },
+        {
+            "name": "Downtown Theater Studio",
+            "rating": 4.7,
+            "location": {
+                "country": "United States",
+                "city": "Los Angeles"
+            },
+            "image_url": "https://cdn.britannica.com/97/100097-050-7E411D6F/Harbor-Freeway-Los-Angeles.jpg",
+            "date": "2023-05-12",
+            "price": 150.25,
+            "latitude": 34.053690, 
+            "longitude": -118.242760
+        },
+        {
+            "name": "Botanical Garden Retreat",
+            "rating": 4.3,
+            "location": {
+                "country": "France",
+                "city": "Paris"
+            },
+            "image_url": "https://i.insider.com/649b13386eb0a800194d589a?width=700",
+            "date": "2023-06-18",
+            "price": 112.99,
+            "latitude": 48.856610,
+            "longitude": 2.351490
+        },
+        {
+            "name": "Sunset Beach House",
+            "rating": 4.6,
+            "location": {
+                "country": "United States",
+                "city": "Miami"
+            },
+            "image_url": "https://a.cdn-hotels.com/gdcs/production156/d397/81ce7a3b-5be9-4d0c-89e2-ab7920d94bb9.jpg?impolicy=fcrop&w=800&h=533&q=medium",
+            "date": "2023-07-22",
+            "price": 180,
+            "latitude": 25.774270,
+            "longitude": -80.193659
+        },
+        {
+            "name": "Contemporary Art Gallery Loft",
+            "rating": 4.4,
+            "location": {
+                "country": "Italy",
+                "city": "Rome"
+            },
+            "image_url": "https://elcomercio.pe/resizer/moFP7riBzwBVAfj261qNbHuxtc8=/980x0/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/BH23GG3F2ZAITPGW4BC2CLCV64.jpg",
+            "date": "2023-08-30",
+            "price": 168.75,
+            "latitude": 41.902330,
+            "longitude": 12.453250 
+        },
+        {
+            "name": "Nature Reserve Cabin",
+            "rating": 4.1,
+            "location": {
+                "country": "Brazil",
+                "city": "Rio de Janeiro"
+            },
+            "image_url": "https://whc.unesco.org/uploads/thumbs/site_1100_0004-750-750-20120625114004.jpg",
+            "date": "2023-09-14",
+            "price": 70.5,
+            "latitude": -22.908330,
+            "longitude": -43.196388
+        },
+        {
+            "name": "Bohemian Café Getaway",
+            "rating": 4.9,
+            "location": {
+                "country": "Spain",
+                "city": "Barcelona"
+            },
+            "image_url": "https://d3dqioy2sca31t.cloudfront.net/Projects/cms/production/000/020/484/large/d0531471711b367b94abfd4dbc29e6ae/spain-barcelona-sagrada-familia-080416-az.jpg",
+            "date": "2023-10-05",
+            "price": 89.99,
+            "latitude": 41.390205, 
+            "longitude": 2.154007
+        }
+    ]
+}
+
+
